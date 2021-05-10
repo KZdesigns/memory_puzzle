48abcc23dd8cd196b90c3b79c6081a33ebc4c507
Kyle Zimmerman <kzdesignsios@gmail.com>
2021-05-09 17:27:49 -0700

    card class complete

---
+++ card.rb
@@ -0,0 +1,43 @@
+class Card # creating a single card object
+    VALUES = ("A".."Z").to_a
+
+    def self.shuffled_pairs(num_pairs)
+        values = VALUES
+
+        while num_pairs > values.length
+            values = values + values
+        end
+
+        values = values.shuffle.take(num_pairs) * 2
+        values.shuffle!
+        values.map { |val| self.new(val) }
+    end
+
+    attr_reader :value
+
+    def initialize(value, revealed = false) 
+        @value = value
+        @revealed = revealed
+    end
+
+    def hide
+        @revealed = false
+    end
+
+    def reveal
+        @revealed = true
+    end
+
+    def to_s
+        revealed? ? value.to_s : " " 
+    end
+
+    def revealed?
+        @revealed
+    end
+
+    def ==(object)
+        object.is_a?(self.class) && object.value == value
+    end
+
+end
